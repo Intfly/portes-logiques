126591dd7c939bc3cbca434ae8139a4a13e7e1d0
Int <71024388+Intfly@users.noreply.github.com>
2022-10-02 01:28:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@
 modélisation du passage de courrant par un booléen
 """
 
-from tkinter import N
-
-
 class Gate:
     def __init__(self,valeur_bouleenne: bool):
         self.booleen: bool = valeur_bouleenne
@@ -28,8 +25,8 @@ class Gate:
         """
 
     def _logic_nor(self, Q):
-        if Gate(self.booleen).logic_not().booleen:           
-            if Gate(Q.booleen).logic_not().booleen:
+        if Gate(self.booleen)._logic_not().booleen:           
+            if Gate(Q.booleen)._logic_not().booleen:
                 return Gate(True)     
         return Gate(False)
         """
@@ -38,60 +35,74 @@ class Gate:
         """
 
     def _logic_and(self,Q):
-        A = self.logic_nor(self)
-        B = Q.logic_nor(Q)
-        return Gate(A.logic_nor(B).booleen)
+        A = self._logic_nor(self)
+        B = Q._logic_nor(Q)
+        return Gate(A._logic_nor(B).booleen)
 
     def _logic_or(self,Q):
-        A = self.logic_nand(self)
-        B = Q.logic_nand(Q)
-        return Gate(A.logic_nand(B).booleen)
+        A = self._logic_nand(self)
+        B = Q._logic_nand(Q)
+        return Gate(A._logic_nand(B).booleen)
 
     def _logic_xor(self,Q):
-        A = self.logic_and(Q.logic_not())
-        B = Q.logic_and(self.logic_not())
-        return Gate(A.logic_or(B).booleen)
+        A = self._logic_and(Q._logic_not())
+        B = Q._logic_and(self._logic_not())
+        return Gate(A._logic_or(B).booleen)
 
     def _logic_xnor(self,Q):
-        return Gate(self.logic_xor(Q).logic_not().booleen)
+        return Gate(self._logic_xor(Q)._logic_not().booleen)
 
     def circuit_additionneur(self,Q,Cin):
-        E1 = self.logic_xor(Q)
-        S = E1.logic_xor(Cin).booleen
-        E2 = E1.logic_and(Cin)
-        E3 = self.logic_and(Q)
-        Cout = E2.logic_or(E3).booleen
+        E1 = self._logic_xor(Q)
+        S = E1._logic_xor(Cin).booleen
+        E2 = E1._logic_and(Cin)
+        E3 = self._logic_and(Q)
+        Cout = E2._logic_or(E3).booleen
         return Cout, S
     
 class Nombre(Gate):
     
-    def __init__(self,Nombre):
-        self.nombre = self._binaire(Nombre)
+    def __init__(self,Nombre,est_binaire):
+        if est_binaire:
+            self.nombre = Nombre
+        else:
+            self.nombre = self._versBinaire(Nombre)
 
-    def _binaire(self, Nombre):
-        binaire :int =0
-        while Nombre  > 0:
-            n=0
-            while 2**n < Nombre:
-                n+=1
-            Nombre -=(2**n)
-            binaire += 1+(0*10**(n-1))
+    def _versBinaire(self, Nombre):
+        binaire:str = ""
+        for i in range(8):
+            if Nombre >= 2**(8-i-1):
+                binaire+="1"
+                Nombre -=(2**(8-i-1))
+            else:
+                binaire+="0"
         return binaire
 
-P = Gate(True)
-Q = Gate(False)
-Cin = Gate(False)
-#print(P.logic_not())
-#print(P.logic_nand(Q))
-#print(P.logic_nor(Q))
-#print(P.logic_and(Q))
-#print(P.logic_or(Q))
-#print(P.logic_xor(Q))
-#print(P.logic_xnor(Q))
+    def addition(self,Nombre2):
+        N1 = self.nombre
+        N2 = Nombre2.nombre
+        N3 = "00000000"
+        for i in range(8):
+            N3[len(N3)-i-1] = Gate(bool(N1[len(N1)-i-1])).circuit_additionneur(Gate(bool(N2[len(N2)-i-1])),Gate(bool(N3[len(N3)-i-1])))
+        return N3
+
+
+
+P: Gate = Gate(True)
+Q: Gate = Gate(False)
+Cin: Gate = Gate(False)
+#print(P._logic_not())
+#print(P._logic_nand(Q))
+#print(P._logic_nor(Q))
+#print(P._logic_and(Q))
+#print(P._logic_or(Q))
+#print(P._logic_xor(Q))
+#print(P._logic_xnor(Q))
 #print(P.circuit_additionneur(Q, Cin))
 
-N1 = Nombre(5)
-print(N1.nombre)
+N1: Nombre = Nombre(15,False)
+N2: Nombre = Nombre("0010010",True)
+print(N2.addition(N1))
 
 
  
