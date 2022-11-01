53122121a5169a77a8a2081213ace921a14f5fb9
Int <71024388+Intfly@users.noreply.github.com>
2022-11-01 17:46:27 +0100

    commit

--- interface.py
+++ interface.py
@@ -1,10 +1,8 @@
-from tkinter.ttk import Style
-from turtle import bgcolor, onclick
-from flet import Page,Text,Row,Column,ElevatedButton,TextButton,TextField,AppBar,View,colors,alignment,margin,Container,padding,ButtonStyle,Image,IconButton,icons
+from flet import Page,Text,Row,Column,ElevatedButton,TextButton,TextField,AppBar,View,colors,alignment,margin,Container,padding,ButtonStyle,Image,IconButton,icons,UserControl
 from nombre import *
 
 def main(page : Page):
-    page.window_width = 700
+    page.window_width = 703
     page.window_height = 550
     page.theme_mode = "light"
     def route_change(route):
@@ -43,11 +41,11 @@ def main(page : Page):
         if page.route == "/application1":
             entree1 = TextField(hint_text="Nombre 1")
             entree2 = TextField(hint_text="Nombre 2")
-            resultat = Text(value="")
+            resultat_add = Text(value="")
             def test(e):
                 nombre1 = Nombre(int(entree1.value),False)
                 nombre2 = Nombre(int(entree2.value),False)
-                resultat.value=f"résultat= {nombre1+nombre2}"
+                resultat_add.value=f"résultat= {nombre1+nombre2}"
                 page.update()
             
             page.views.append(
@@ -56,44 +54,60 @@ def main(page : Page):
                     [
                         AppBar(title=Text("Store"), bgcolor=colors.SURFACE_VARIANT),
                         ElevatedButton("Menu", on_click=lambda _: page.go("/")),
-                        Column(controls=[Row(controls=[entree1,entree2]),TextButton(text="addition",on_click=test),resultat]),
+                        Column(controls=[Row(controls=[entree1,entree2]),TextButton(text="addition",on_click=test),resultat_add]),
                     ],
                 )
             )
-        if page.route == "/calculatrice":
-            resultat= Text(value="out= A+B",size=30)
-            def gate(nom,etat,texte):
-                if etat:
-                    return Container(content=Image(src=f"\calc\{nom}.png"),width=67,height=90,bgcolor="#10000000",border_radius=10,padding=padding.symmetric(horizontal=10),alignment=alignment.center,)
-                if texte:
-                    return Container(content=Text(value=nom,size=40),width=67,height=90,bgcolor="#10000000",border_radius=10,padding=padding.symmetric(horizontal=10),alignment=alignment.center,)
-                return Text(value=f"{nom}2")
-
-            switch_symbol= Text("mode")
-            tableau_boutons= [gate("not",True,False),gate("or",True,False),gate("and",True,False),gate("A",False,True),gate("B",False,True),gate("C",False,True),gate("xnor",True,False),gate("xor",True,False),gate("nor",True,False),gate("nand",True,False),gate("(",False,True),gate(")",False,True)]
-            def changer_mode(e):
-                e.control.selected = not e.control.selected
-                e.control.update()
-            page.views.append(
-                View("/calculatrice",
-                [
-                    Column(controls=[
-                    Container(content=TextButton(content=Image(src="\_bouton_retour.png"),width=50,height=50,on_click=lambda _:page.go("/")),padding=padding.only(left=10,top=10)),
-                    Container(content=resultat,margin=margin.only(top=30,left=20,bottom=40,right=10)),
-                    Container(height=1.5,width=700,bgcolor="#78000000"),
-                    Row(controls=[
-                        Container(content=switch_symbol,margin=margin.only(left=30)),
-                        IconButton(icon=icons.BATTERY_1_BAR,selected_icon=icons.BATTERY_FULL,selected=False,on_click=changer_mode,tooltip="changer le mode de représentation"),
-                        Container(TextButton("calculer",
-                            style = ButtonStyle(
-                                bgcolor="#3283FD",
-                                color="#FFFFFF",
-                                padding=padding.symmetric(vertical=3,horizontal=20)),
-                            on_click=lambda _: page.go("/calculatrice")),margin=margin.only(left=450))
-                        ],),
-                    Container(height=1.5,width=700,bgcolor="#78000000"),
-                    Container(content=Row(controls=tableau_boutons,wrap=True,spacing=37,run_spacing=14),padding=padding.symmetric(horizontal=50,vertical=20))
-            ])],padding=padding.only(),))
+        if page.route == "/calculatrice":#vue sous la forme d'une classe
+            class calculatrice(UserControl):
+                def __init__(self):
+                    self.resultat= Text(value="out= A+B",size=30)
+                    self.calcul=""
+                    self.switch_symbol= Text("mode")
+                
+                def action_boutons(self,e):
+                    self.resultat.value=e.control.data
+                    print(f"e.data:{e.data}")
+                    page.update()
+                def gate(self,nom,etat,texte):
+                    if etat:
+                        return TextButton(
+                            content=Container(content=Image(src=f"\calc\{nom}.png"),width=67,height=90,bgcolor="#10000000",border_radius=10,padding=padding.symmetric(horizontal=10),alignment=alignment.center,),
+                            on_click=self.action_boutons,
+                            style=ButtonStyle(padding=padding.only(),overlay_color="#00000000"),
+                            data=0)
+                    if texte:
+                        return TextButton(
+                            content=Container(content=Text(value=nom,size=40),width=67,height=90,bgcolor="#10000000",border_radius=10,padding=padding.symmetric(horizontal=10),alignment=alignment.center,),
+                            on_click=self.action_boutons,
+                            style=ButtonStyle(padding=padding.only(),color="#DA000000",overlay_color="#00000000"),
+                            data=nom)
+                    return Text(value=f"{nom}2")
+                def changer_mode(e):
+                    e.control.selected = not e.control.selected
+                    e.control.update()
+                def calc(self):
+                    tableau_boutons= [self.gate("not",True,False),self.gate("or",True,False),self.gate("and",True,False),self.gate("A",False,True),self.gate("B",False,True),self.gate("C",False,True),self.gate("xnor",True,False),self.gate("xor",True,False),self.gate("nor",True,False),self.gate("nand",True,False),self.gate("(",False,True),self.gate(")",False,True)]
+                    return View("/calculatrice",
+                        [
+                            Column(controls=[
+                            Container(content=TextButton(content=Image(src="\_bouton_retour.png"),width=50,height=50,on_click=lambda _:page.go("/")),padding=padding.only(left=10,top=10)),
+                            Container(content=self.resultat,margin=margin.only(top=30,left=20,bottom=40,right=10)),
+                            Container(height=1.5,width=700,bgcolor="#78000000"),
+                            Row(controls=[
+                                Container(content=self.switch_symbol,margin=margin.only(left=30)),
+                                IconButton(icon=icons.BATTERY_1_BAR,selected_icon=icons.BATTERY_FULL,selected=False,on_click=self.changer_mode,tooltip="changer le mode de représentation"),
+                                Container(TextButton("calculer",
+                                    style = ButtonStyle(
+                                        bgcolor="#3283FD",
+                                        color="#FFFFFF",
+                                        padding=padding.symmetric(vertical=3,horizontal=20)),
+                                    on_click=lambda _: page.go("/calculatrice")),margin=margin.only(left=450))
+                                ],),
+                            Container(height=1.5,width=700,bgcolor="#78000000"),
+                            Container(content=Row(controls=tableau_boutons,wrap=True,spacing=37,run_spacing=14),padding=padding.symmetric(horizontal=50,vertical=20))
+                    ])],padding=padding.only())
+            page.views.append(calculatrice().calc())
         page.update()
 
     def view_pop(view):
